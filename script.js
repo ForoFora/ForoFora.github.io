55e7eb8edb7010e0c2741533a7f5db03ac538068
Vladimir P <kagami.vp@gmail.com>
2020-03-16 21:51:01 +0300

    Add new block "steps"

---
+++ script.js
@@ -0,0 +1,17 @@
+let stpblt1 = document.getElementById("stepbl-1")
+let stpbrd1 = document.getElementById("stepbdr-1")
+
+stpblt1.onmouseover = () => stpbrd1.style.borderBottom = "3px solid #fff"
+stpblt1.onmouseout = () => stpbrd1.style.borderBottom = "3px solid #000"
+
+let stpblt2 = document.getElementById("stepbl-2")
+let stpbrd2 = document.getElementById("stepbdr-2")
+
+stpblt2.onmouseover = () => stpbrd2.style.borderBottom = "3px solid #fff"
+stpblt2.onmouseout = () => stpbrd2.style.borderBottom = "3px solid #000"
+
+let stpblt3 = document.getElementById("stepbl-3")
+let stpbrd3 = document.getElementById("stepbdr-3")
+
+stpblt3.onmouseover = () => stpbrd3.style.borderBottom = "3px solid #fff"
+stpblt3.onmouseout = () => stpbrd3.style.borderBottom = "3px solid #000"
